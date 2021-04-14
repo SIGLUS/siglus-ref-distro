168e9708d31b8f29a1278101eeeda0ffe2c2d70d
yinshangwei <swyin@thoughtworks.com>
2021-04-14 14:34:11 +0800

    [shangwei] #N/A update shell.

--- restart-prod-services.sh
+++ restart-prod-services.sh
@@ -8,7 +8,7 @@ echo ">>>>>>>>> deregister consul"
 export CONSUL_HOST=10.1.0.81:8500
 for SERVICE_NAME in auth fulfillment notification reference-ui referencedata report requisition siglusapi stockmanagement
 do
-    echo "deregister service: "$SERVICE_NAME
+    echo "Deregistering service siglus_"$SERVICE_NAME
     curl -s http://$CONSUL_HOST/v1/health/service/$SERVICE_NAME | jq -r '.[] | "curl -XPUT http://$CONSUL_HOST/v1/agent/service/deregister/" + .Service.ID' >> clear.sh
 done
 chmod a+x clear.sh && ./clear.sh
